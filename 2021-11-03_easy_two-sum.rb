b192bcb8238ccfba00aa6c593466d8995285e7a6
Rod McNew <rmcnew@relivinc.com>
2021-11-03 20:10:06 -0500

    Create 2021-11-03_easy_two-sum.rb

---
+++ 2021-11-03_easy_two-sum.rb
@@ -0,0 +1,22 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    p nums.class
+    nums = nums.map.with_index {|num,i| [i, num]}
+    nums = nums.sort_by { |item| item[1] }
+    left = 0
+    right = nums.length - 1
+    while left < right
+        sum = nums[left][1] + nums[right][1]
+        if sum > target
+            puts 'right -= 1'
+            right -= 1
+        elsif sum < target
+            puts 'left += 1'
+            left += 1
+        else
+            return [nums[left][0], nums[right][0]]
+        end
+    end
+end
